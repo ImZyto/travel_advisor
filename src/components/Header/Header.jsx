d0e61aa600d2326e7aa2cd7bd269b01027f815d9
Michal Zybura <michal.a.zybura@gmail.com>
2022-02-07 18:57:52 +0100

    create_list

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -23,7 +23,7 @@ const Header = ({ onPlaceChanged, onLoad }) => {
               <div className={classes.searchIcon}>
                 <SearchIcon />
               </div>
-              <InputBase placeholder="Search…" classes={{ root: classes.inputRoot, input: classes.inputInput }} />
+              <InputBase placeholder="Szukaj…" classes={{ root: classes.inputRoot, input: classes.inputInput }} />
             </div>
           {/* </Autocomplete> */}
         </Box>
--- src/components/List/List.jsx
+++ src/components/List/List.jsx
@@ -1,9 +1,41 @@
-import React from 'react';
+import React, { useState, useEffect, createRef } from 'react';
+import { CircularProgress, Grid, Typography, InputLabel, MenuItem, FormControl, Select } from '@material-ui/core';
+
+//import PlaceDetails from '../PlaceDetails/PlaceDetails';
+import useStyles from './styles.js';
 
 const List = () => {
-return (
-    <h1>List</h1>
-);
+  //const [elRefs, setElRefs] = useState([]);
+  const classes = useStyles();
+  const [type, setType] = useState('restaurants');
+  const [rating, setRating] = useState('');
+
+//   useEffect(() => {
+//     setElRefs((refs) => Array(places.length).fill().map((_, i) => refs[i] || createRef()));
+//   }, [places]);
+
+  return (
+    <div className={classes.container}>
+      <Typography variant="h4">Restauracje, hotele i atrakcje wokół Ciebie</Typography>
+          <FormControl className={classes.formControl}>
+            <InputLabel id="type">Typ</InputLabel>
+            <Select id="type" value={type} onChange={(e) => setType(e.target.value)}>
+              <MenuItem value="restaurants">Restauracje</MenuItem>
+              <MenuItem value="hotels">Hotele</MenuItem>
+              <MenuItem value="attractions">Attrakcje</MenuItem>
+            </Select>
+          </FormControl>
+          <FormControl className={classes.formControl}>
+            <InputLabel id="rating">Ocena</InputLabel>
+            <Select id="rating" value={rating} onChange={(e) => setRating(e.target.value)}>
+              <MenuItem value="">Wszystkie</MenuItem>
+              <MenuItem value="3">Powyżej 3.0</MenuItem>
+              <MenuItem value="4">Powyżej 4.0</MenuItem>
+              <MenuItem value="4.5">Powyżej 4.5</MenuItem>
+            </Select>
+          </FormControl>
+    </div>
+  );
 };
 
 export default List;
